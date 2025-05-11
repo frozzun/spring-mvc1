332a4d35245f4891f31b9d9c709f33c59b68ba09
frozzun <eonjun0783@naver.com>
2025-05-11 18:39:16 +0900

    로깅 간단히 알아보기

---
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+  @RequestMapping("/log-test")
+  public String logTest() {
+    String name = Thread.currentThread().getName();
+
+    log.trace("trace log={}", name);
+    log.info("info log={}", name);
+
+    return "ok";
+  }
+}
